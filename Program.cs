6796ad586a2b9a8c6a4d1feb2b03c045e4df93f7
Kirti Kumar <kirtikumar237823@gmail.com>
2026-08-04 13:50:17 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number :");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int temp = number;
+            int reverse = 0;
+
+            while(number > 0)
+            {
+                int digit = number % 10;
+                reverse = reverse * 10 + digit;
+                number = number / 10;
+
+            }
+            if(temp == reverse)
+            {
+                Console.WriteLine("Palindrome");
+            } else
+            {
+                Console.WriteLine("Not Palindrome");
+            }
+        }
+    }
+}
