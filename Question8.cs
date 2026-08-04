4e6ab37069109e6df36983dd73e3e33e1b37a442
Kirti Kumar <kirtikumar237823@gmail.com>
2026-08-04 14:20:49 +0530

    Add files via upload

---
+++ Question8.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Question8
+{
+    internal class Question8
+    {
+        static void Main2(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            bool isPrime = true;
+
+            if (number <= 1)
+            {
+                isPrime = false;
+            }
+            else
+            {
+                for (int i = 2; i <= Math.Sqrt(number); i++)
+                {
+                    if (number % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+
+            if (isPrime)
+                Console.WriteLine("Prime Number");
+            else
+                Console.WriteLine("Non Prime Number");
+        }
+    }
+}
