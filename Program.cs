aaa6748a29098358bc09e60b547f2f57f4f08d56
Kirti Kumar <kirtikumar237823@gmail.com>
2026-08-04 14:17:41 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Question7
+{
+    static void Main5()
+    {
+        Console.Write("Enter a positive integer: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        long factorial = 1;
+
+        for (int i = 1; i <= num; i++)
+        {
+            factorial = factorial * i;
+        }
+
+        Console.WriteLine("Factorial of " + num + " is: " + factorial);
+    }
+}
