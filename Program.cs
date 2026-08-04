802d3997faa6ab85e74b1b9287e1ac167b7c9b52
Kirti Kumar <kirtikumar237823@gmail.com>
2026-08-04 14:11:20 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Question5
+
+{
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("\nChoose an Operation:");
+        Console.WriteLine("1. Addition");
+        Console.WriteLine("2. Subtraction");
+        Console.WriteLine("3. Multiplication");
+        Console.WriteLine("4. Division");
+
+        Console.Write("Enter your choice (1-4): ");
+        int choice = Convert.ToInt32(Console.ReadLine());
+
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine("Result = " + (num1 + num2));
+                break;
+
+            case 2:
+                Console.WriteLine("Result = " + (num1 - num2));
+                break;
+
+            case 3:
+                Console.WriteLine("Result = " + (num1 * num2));
+                break;
+
+            case 4:
+                if (num2 != 0)
+                    Console.WriteLine("Result = " + (num1 / num2));
+                else
+                    Console.WriteLine("Cannot divide by zero.");
+                break;
+
+            default:
+                Console.WriteLine("Invalid Choice!");
+                break;
+        }
+    }
+}
